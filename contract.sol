4b58a01d2bca01711878bcfe451ec42445042591
vmwhoami <vitalie.m@travod.com>
2022-06-11 00:06:42 +0300

    Complete generateRandom dna private function

--- contract.sol
+++ contract.sol
@@ -16,7 +16,8 @@ contract ZombieFactory {
          zombies.push(Zombie(_name, _dna));
     }
 
-    function _generateRandomDna(string memory _str) private view returns(uint) {
-
+    function _generateRandomDna(string memory _str) private view returns (uint) {
+       uint rand = uint(keccak256(abi.encodePacked(_str)));
+       return rand % dnaModulus;
     }
 }
