0a20a22cd71c07bfb16b9547a115ad4a1209b9d8
vmwhoami <vitalie.m@travod.com>
2022-06-12 16:33:04 +0300

    Add zombieToOwner  ownerZombieCount mapping

--- contract.sol
+++ contract.sol
@@ -14,6 +14,10 @@ contract ZombieFactory {
     }
 
     Zombie[] public zombies;
+    
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
+
 
     function _createZombie(string memory _name, uint _dna) private {  
         zombies.push(Zombie(_name, _dna));
