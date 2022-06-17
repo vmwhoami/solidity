aa594b7f6dae9cfdcda99b700143c4cb2d0b11c5
vmwhoami <vitalie.m@travod.com>
2022-06-18 00:30:11 +0300

    Add feed and multiply function

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -4,5 +4,8 @@ pragma solidity >=0.6.0 <0.9.0;
 import "./zombiefactory.sol";
 
 contract ZombieFeeding is ZombieFactory {
+    function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+    
+  }
     
 }
