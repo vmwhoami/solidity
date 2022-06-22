867769c81ec27d01ace9957bcbbb2cee1f4b30f1
vmwhoami <vitalie.m@travod.com>
2022-06-22 22:34:38 +0300

    Add zombie.id to Zombie

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -5,7 +5,8 @@ import "./zombiefactory.sol";
 
 contract ZombieFeeding is ZombieFactory {
     function feedAndMultiply(uint _zombieId, uint _targetDna) public {
-    
+      require(msg.sender == zombieToOwner[_zombieId]);
+      Zombie storage myZombie = zombies[_zombieId];
   }
     
 }
