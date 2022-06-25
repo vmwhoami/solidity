10732325112be39c7d2eede140a54ef19f3afc3e
vmwhoami <vitalie.m@travod.com>
2022-06-25 18:32:54 +0300

    Learn about message sender!!!

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -5,6 +5,8 @@ import "./zombiefactory.sol";
 
 contract ZombieFeeding is ZombieFactory {
     function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+      // msg.sender a person who's currently connecting with the contract.
+      // read this! https://medium.com/coinmonks/solidity-who-the-heck-is-msg-sender-de68d3e98454
       require(msg.sender == zombieToOwner[_zombieId]);
       Zombie storage myZombie = zombies[_zombieId];
   }
